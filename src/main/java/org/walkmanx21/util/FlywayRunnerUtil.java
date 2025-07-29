2f1ca2108b58f3887b0474606a711823422add19
walkmanx21 <walkmanx21@gmail.com>
2025-07-29 20:19:32 +0300

    Подготовлен скрип для инициализации БД через Flyway

---
+++ src/main/java/org/walkmanx21/util/FlywayRunnerUtil.java
@@ -0,0 +1,6 @@
+package org.walkmanx21.util;
+
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.MigrationInfo;
+import org.flywaydb.core.api.output.MigrateResult;
+import org.springframework.stereotype.Component;
