1753e9efa10921fa04bea648c8c9577501e4bcf3
walkmanx21 <walkmanx21@gmail.com>
2025-07-31 14:33:59 +0300

    добавлены пул-соединений Hikari CP

--- src/main/java/org/walkmanx21/config/FlywayConfig.java
+++ src/main/java/org/walkmanx21/config/FlywayConfig.java
@@ -1,7 +1,8 @@
 package org.walkmanx21.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
-import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,9 @@ import javax.sql.DataSource;
 
 @Configuration
 public class FlywayConfig {
+
+    @Value("${db.driver}")
+    private String databaseDriver;
 
     @Value("${db.url}")
     private String databaseUrl;
@@ -19,13 +23,32 @@ public class FlywayConfig {
     @Value("${db.password}")
     private String databasePassword;
 
+    @Value("${db.hikari.maximumPoolSize}")
+    private int maxPoolSize;
+
+    @Value("${db.hikari.minimumIdle}")
+    private int minIdle;
+
+    @Value("${db.hikari.idleTimeout}")
+    private long idleTimeout;
+
+    @Value("${db.hikari.poolName}")
+    private String poolName;
+
     @Bean
     public DataSource dataSource() {
-        PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setUrl(databaseUrl);
-        ds.setUser(databaseUser);
-        ds.setPassword(databasePassword);
-        return ds;
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName(databaseDriver);
+        config.setJdbcUrl(databaseUrl);
+        config.setUsername(databaseUser);
+        config.setPassword(databasePassword);
+        config.setMaximumPoolSize(maxPoolSize);
+        config.setMinimumIdle(minIdle);
+        config.setIdleTimeout(idleTimeout);
+        config.setPoolName(poolName);
+
+        return new HikariDataSource(config);
     }
 
     @Bean(initMethod = "migrate")
