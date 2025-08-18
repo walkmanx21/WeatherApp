2bcf7a84f7daa72e3f71be78e31b12d032ba5c37
walkmanx21 <walkmanx21@gmail.com>
2025-08-18 15:53:44 +0300

    рефакторинг ExceptionHandler'а: дополнена реализация кастомных сообщений во вью

--- src/main/java/org/walkmanx21/util/ExceptionHandlingFilterUtil.java
+++ src/main/java/org/walkmanx21/util/ExceptionHandlingFilterUtil.java
@@ -1,14 +1,11 @@
 package org.walkmanx21.util;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-import org.walkmanx21.dto.ErrorResponseDto;
 import org.walkmanx21.exceptions.BadRequestForWeatherApiServiceException;
-import org.walkmanx21.exceptions.LocationAlreadyExistException;
 import org.walkmanx21.exceptions.StorageUnavailableException;
 import org.walkmanx21.exceptions.WeatherApiServiceUnavailableException;
 
@@ -22,12 +19,18 @@ public class ExceptionHandlingFilterUtil {
     })
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView handleExceptionsForRedirectView (Exception e) {
-        return new ModelAndView("/error");
+        return buildModelAndView("We're sorry, but an unexpected error has occurred. Please try again later.");
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ModelAndView handleException(Exception e) {
-        return new ModelAndView("/error");
+        return buildModelAndView("The requested page was not found");
+    }
+
+    private ModelAndView buildModelAndView(String message) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", message);
+        return modelAndView;
     }
 }
